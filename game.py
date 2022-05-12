de7cb45f4d4437acf05578de12f15d044cb4d161
rmurry <rylanmurry@gmail.com>
2022-05-11 19:03:06 -0500

    added END event and framerate

--- game.py
+++ game.py
@@ -1,18 +1,30 @@
+import pygame
 from player import Player
+from game_state import GameState
 
 class Game:
     def __init__(self,screen):
         self.screen = screen
         self.objects = []
+        self.game_state = GameState.NONE
     
     def set_up(self):
         player = Player()
         self.objects.append(player)
         print("do set up")
+        self.game_state = GameState.RUNNING
 
 
     def update(self):
         print("update")
 
+        self.handle_events()
+
         for object in self.objects:
             object.render(self.screen)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game_state = GameState.ENDED
+                
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-import config                   # config.py file in directory/repo
-from game import Game           # game.py file in directory/repo
+import config                       # config.py file in directory/repo
+from game import Game               # game.py file in directory/repo
+from game_state import GameState    # game_state.py file in directory/repo
 
 pygame.init()
 
@@ -8,10 +9,13 @@ screen = pygame.display.set_mode((600,400))
 
 pygame.display.set_caption('Top Scroller')
 
+
+clock = pygame.time.Clock()         #adds framerate to the game
 game = Game(screen)
 game.set_up()
 
-while True:
+while game.game_state == GameState.RUNNING:
+    clock.tick(60)
     game.update()
     screen.fill(config.BLACK)
     pygame.display.flip()
