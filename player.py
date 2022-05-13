294b1d86c0a5783623fedcdf716cd73411c4caa4
rmurry <rylanmurry@gmail.com>
2022-05-13 11:16:15 -0500

    map changes and added some sprites

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Player:
     def __init__(self,x_pos,y_pos):
         print("player created")
         self.position = [x_pos,y_pos]
-        self.image = pygame.image.load("imgs/player.png")
+        self.image = pygame.image.load("imgs/rylan.png")
         self.image = pygame.transform.scale(self.image,(config.SCALE,config.SCALE))
         self.rect = pygame.Rect(self.position[0]*config.SCALE,self.position[1]*config.SCALE,config.SCALE,config.SCALE)
 
